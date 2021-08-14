1bad745ff6649fcebfc659a1b41011df6aaab7be
Anand Kumar Martund <python.marthanda@gmail.com>
2021-08-14 19:52:26 +0530

    Clean workspace

--- docker_module/modules/common/main.tf
+++
@@ -1,5 +0,0 @@
-#resources
-
-resource "docker_network" "wordpress_net" {
-  name = var.docker_network
-}
--- docker_module/modules/common/variables.tf
+++
@@ -1,6 +0,0 @@
-# Input variable definitions
-
-variable "docker_network" {
-    description = "Docker network"
-    type        = string
-}
